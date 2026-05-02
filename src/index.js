5148e2c3f617ce7005274cf5e66b6e512a99cf52
Muhammad Qasim <mrqasimhaleem@gamil.com>
2026-05-02 15:41:09 +0500

    database connected sucssfully

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require('dotenv').config({})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+/*
+import express from 'express'
+const app = express()
+(async ()=> {
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error) => {
+        console.log(
+            "application not running"
+        )
+        throw error
+       })
+
+       app.listen(process.env.PORT, () => {
+        console.log(`app listening on port ${process.env.PORT}`)
+       })
+    }
+    catch(err) {
+        console.log(err)
+        throw err
+
+    }
+
+})()
+    */
